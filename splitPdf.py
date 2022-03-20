15e289d5864e00d2ca486efbbb6ffcdfbf40ec1f
Mehmet Emre Doğan <87245315+Mehmet-Emre-Dogan@users.noreply.github.com>
2022-03-21 01:57:45 +0300

    bugfix
    in some versions the __file__ thing returns full path. taken care of that

--- splitPdf.py
+++ splitPdf.py
@@ -38,7 +38,10 @@ if __name__ == "__main__":
     args = sys.argv
     if DEBUG:
         print(args)
-    args.remove(__file__)
+        print(f"file: {__file__}")
+    relativeFile = str(__file__).split("\\")[-1]
+    if ".py" in relativeFile:
+        args.remove(relativeFile)
     if DEBUG:
         print(args)
     if len(args) < 3:
